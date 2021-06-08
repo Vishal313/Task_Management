b8e0b64c813194906afa8ea0d7728a120f0c5730
Vishal313 <palaivishal@gmail.com>
2021-06-08 20:49:00 +0530

    Test 1

---
+++ src/main/java/com/tasking/Task_Management/App.java
@@ -0,0 +1,13 @@
+package com.tasking.Task_Management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
