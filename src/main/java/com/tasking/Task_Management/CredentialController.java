356c8e755e08cb3d5419f6927611cef12af19db8
Vishal313 <palaivishal@gmail.com>
2021-06-15 10:43:09 +0530

    Credentials

--- src/main/java/com/tasking/Task_Management/CredentialController.java
+++ src/main/java/com/tasking/Task_Management/CredentialController.java
@@ -1,7 +1,12 @@
 package com.tasking.Task_Management;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.ServletActionContext;
 
@@ -17,26 +22,62 @@ public class CredentialController {
 	public String execute() {
 		method = ServletActionContext.getRequest().getMethod();
 		try {
-			if (method.equals("GET")) {
-				System.out.println("I am gett!!");
-				CredentialRepository credRep = new CredentialRepository();
-				ArrayList<Credential> credList = credRep.findAllCredential();
-				response.put("credList", credList);
-			}
 			if (method.equals("POST")) {
 				System.out.println("I am post!!");
-				CredentialRepository credRep = new CredentialRepository();
-				String empid = ServletActionContext.getRequest().getParameter("empid");
-				String usrnm = ServletActionContext.getRequest().getParameter("usrnm");
-				String pswd = ServletActionContext.getRequest().getParameter("pswd");
-				credRep.createNewCredential(5, usrnm, pswd);
-			}
-			
+				checkCredentials();
+			}			
 		} catch (Exception e) {
 			e.printStackTrace();
+		}		
+		return "success";
+	}
+	
+	public void checkCredentials() {
+		HttpServletRequest request = ServletActionContext.getRequest();
+		HashMap<String,String> map = JDBC.pasrseRequest(request);
+		HttpServletResponse res = ServletActionContext.getResponse();
+		
+		CredentialRepository credRep = new CredentialRepository();
+		ArrayList<Credential> credList = credRep.findAllCredential();
+		boolean loggedIn = false;
+		int employee_id = 0;
+		for (int i = 0; i < credList.size(); i++) {
+			System.out.println();
+			String dbUser = credList.get(i).getUser_name();
+			String dbPass = credList.get(i).getPassword();
+			if (dbUser.equals(map.get("user_name")) && dbPass.equals(getMd5(map.get("password")))) {
+				loggedIn = true;
+				employee_id = credList.get(i).getEmployee_id();
+			}
 		}
 		
+		if (loggedIn) {
+			System.out.println("Login Successfull");
+			response.put("employee_id", employee_id);
+		} else {
+			System.out.println("Login Failed");
+			res.setStatus(401);			
+		}
+		res.setHeader("Access-Control-Allow-Origin", "*");
 		
-		return "success";
+	}
+	
+	public static String getMd5(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			  
+	        byte[] messageDigest = md.digest(input.getBytes());
+	
+	        BigInteger no = new BigInteger(1, messageDigest);
+	
+	        String hashtext = no.toString(16);
+	        while (hashtext.length() < 32) {
+	            hashtext = "0" + hashtext;
+	        }
+	        return hashtext;
+		}
+		catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+		}
 	}
 }
--- src/main/java/com/tasking/Task_Management/JDBC.java
+++ src/main/java/com/tasking/Task_Management/JDBC.java
@@ -1,10 +1,17 @@
 package com.tasking.Task_Management;
 
+import java.io.BufferedReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class JDBC {
 	public static ResultSet getFromDatabase(String query) {
@@ -47,6 +54,21 @@ public class JDBC {
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static HashMap<String,String> pasrseRequest(HttpServletRequest request){
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+		    while ((line = reader.readLine()) != null)
+		    	jb.append(line);
+		} catch (Exception e) {
+			
+		}
+		String jsonString = jb.toString();
+		HashMap<String,String> map = new Gson().fromJson(jsonString, new TypeToken<HashMap<String, String>>(){}.getType());
+		return map;
 	}
 }
 
